12b3c3221393db617edd4b87b27fe99738022bcc
Giovane <giovane.garcia09@gmail.com>
2026-06-20 23:35:28 -0300

    feat: enhance prompt handling in push_prompts.py and pull_prompts.py

--- src/pull_prompts.py
+++ src/pull_prompts.py
@@ -14,10 +14,38 @@ from pathlib import Path
 from dotenv import load_dotenv
 from langsmith import Client
 from utils import save_yaml, check_env_vars, print_section_header
+from langchain_core.prompts import (
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate
+)
 
 load_dotenv()
 
 client = Client()
+
+
+def prompt_to_dict(name: str, prompt: ChatPromptTemplate) -> dict:
+    system_prompt = ""
+    user_prompt = ""
+
+    for msg in prompt.messages:
+        if isinstance(msg, SystemMessagePromptTemplate):
+            system_prompt = msg.prompt.template
+
+        elif isinstance(msg, HumanMessagePromptTemplate):
+            user_prompt = msg.prompt.template
+
+    return {
+        name: {
+            "description": "Prompt para converter relatos de bugs em User Stories",
+            "system_prompt": system_prompt,
+            "user_prompt": user_prompt,
+            "version": "v1",
+            "tags": ["bug-analysis", "user-story", "product-management"],
+            "metadata": prompt.metadata
+        }
+    }
 
 def pull_prompts_from_langsmith():
     print_section_header("Pulling prompts from LangSmith Prompt Hub...")
@@ -26,9 +54,11 @@ def pull_prompts_from_langsmith():
 def main():
     check_env_vars(["LANGSMITH_ENDPOINT", "LANGSMITH_API_KEY", "LANGSMITH_PROJECT"])
     prompt = pull_prompts_from_langsmith()
+    prompt_dict = prompt_to_dict("bug_to_user_story_v1", prompt)
+
     root_dir = Path(__file__).resolve().parent.parent
     prompts_dir = str(root_dir / "prompts" / "bug_to_user_story_v1.yml")
-    saved = save_yaml(prompt, prompts_dir)
+    saved = save_yaml(prompt_dict, prompts_dir)
     if saved:
         print_section_header("Prompt pulled and saved successfully!")
     else:
--- src/push_prompts.py
+++ src/push_prompts.py
@@ -14,12 +14,11 @@ import os
 import sys
 from pathlib import Path
 from dotenv import load_dotenv
-from langchain import hub
+from langsmith import Client
 from langchain_core.prompts import ChatPromptTemplate
 from utils import load_yaml, check_env_vars, print_section_header
 
 load_dotenv()
-
 
 def push_prompt_to_langsmith(prompt_name: str, prompt_data: dict) -> bool:
     """
@@ -33,13 +32,24 @@ def push_prompt_to_langsmith(prompt_name: str, prompt_data: dict) -> bool:
         True se sucesso, False caso contrário
     """
     ...
+    client = Client()
+
+    username = os.getenv("USERNAME_LANGSMITH_HUB")
+    prompt_identifier = f"{username}/{prompt_name}"
+
+    template = ChatPromptTemplate.from_messages(
+        [
+            ("system", prompt_data["system_prompt"]),
+            ("user", prompt_data["user_prompt"])
+        ]
+    )
+
+    print_section_header(f"Pushing prompt '{prompt_identifier}' to LangSmith Hub...")
+    link = client.push_prompt(prompt_identifier, object = template, is_public=True, description=prompt_data["description"], tags=prompt_data["tags"])
+    return link is not None
 
 
 def validate_prompt(prompt_data: dict | None) -> tuple[bool, list]:
-    if prompt_data is None:
-        return False, ["Prompt data is None"]
-    
-    return True, [] 
     """
     Valida estrutura básica de um prompt (versão simplificada).
 
@@ -50,14 +60,32 @@ def validate_prompt(prompt_data: dict | None) -> tuple[bool, list]:
         (is_valid, errors) - Tupla com status e lista de erros
     """
     ...
+    if prompt_data is None:
+        return False, ["Prompt data is None"]
+    
+    required_fields = ["description", "system_prompt", "user_prompt", "version", "tags"]
+    
+    valid = True
+    errors = []
+    for field in required_fields:
+        if field not in prompt_data:
+            valid = False
+            errors.append(f"Missing required field: {field}")
+    
+    return valid, errors
 
 def main():
     check_env_vars(["LANGSMITH_ENDPOINT", "LANGSMITH_API_KEY", "LANGSMITH_PROJECT", "USERNAME_LANGSMITH_HUB"])
     root_dir = Path(__file__).resolve().parent.parent
-    prompts_dir = str(root_dir / "prompts" / "bug_to_user_story_v2.yml")
-    print_section_header(f"Loading prompt from {prompts_dir}...")
-    prompt= load_yaml(prompts_dir)
-    validation_result = validate_prompt(prompt)
+    prompt_name = "bug_to_user_story_v2"
+    prompt_file_name = prompt_name + ".yml"
+
+    prompt_dir = str(root_dir / "prompts" / prompt_file_name)
+    print_section_header(f"Loading prompt from {prompt_dir}...")
+    prompt = load_yaml(prompt_dir)
+    prompt = prompt if prompt is not None else {}
+    v2 = prompt["bug_to_user_story_v2"]
+    validation_result = validate_prompt(v2)
     valid, errors = validation_result
 
     if not valid:
@@ -67,11 +95,8 @@ def main():
         sys.exit(1)
 
     print_section_header("Prompt validation successful. Proceeding to push...")
-    prompt_name = "bug_to_user_story_v2"
 
-    ChatPromptTemplate()
-
-    success = push_prompt_to_langsmith(prompt_name, prompt)
+    success = push_prompt_to_langsmith(prompt_name, v2)
     if success:
         print_section_header("Prompt pushed successfully to LangSmith Hub!")
     else:
